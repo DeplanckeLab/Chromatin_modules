3070fa9d65048ef30aacadbd3a4067464a5eae98
Olga Pushkarev <pushkarev.olga@gmail.com>
2023-10-13 16:00:28 +0200

    VCMtools changes

--- VCMtools.sh
+++ VCMtools.sh
@@ -1,60 +1,33 @@
 #!/bin/bash
-Help()
-{
-   # Display Help
-   echo "Run VCMtools python pipeline."
-   echo "Syntax: VCMtools.sh [-s|d|f|c|p|n|o]"
-}
-
 # +
+## SPECIFY:
+
+## path to VCMtools helper scripts:
+path_to_help=./vcmtools;
+
+## input dataset:
+dataset="test_data";
+
+## path to gzipped BED count matrices
+path_to_input=./${dataset}
+
+## chromosomes to use
+chromosomes="chr21,chr22";
 pvalue=0.001;
 n_cores=1;
 
-# Get the options
-while getopts ":h:s:d:f:c:p:n:o:" option; do
-    case $option in
-        h) # display Help
-            Help
-            exit;;
-        s) scripts_path=${OPTARG};;
-        d) dataset=${OPTARG};;
-        f) path_to_input=${OPTARG};;
-        c) chromosomes=${OPTARG};;
-        p) pvalue=${OPTARG};;
-        n) n_cores=${OPTARG};;
-        o) output_path=${OPTARG};;
-        
-        \?) # Invalid option
-            echo "Error: Invalid option"
-            exit;;
-    esac
-done
+## output path:
+output_path=./VCMtools;
 
-# +
-mkdir -p ${output_path}/corr_files/theoretical_pv_output
-mkdir -p ${output_path}/corr_files/empirical_pv_output
-mkdir -p ${output_path}/CMs
+## Run VCMtools on test_data for chr21 and chr22
+sh ${path_to_help}/vcmtools/run_VCMtools.sh \
+    -s ${path_to_help} \
+    -d ${dataset} \
+    -f ${path_to_input}/H3K27ac_chr21_chr22.bed,${path_to_input}/H3K4me1_chr21_chr22.bed \
+    -c ${chromosomes} \
+    -p ${pvalue} \
+    -n ${n_cores} \
+    -o ${output_path}/${dataset}
+# -
 
-python3.9 \
-    ${scripts_path}/01.get_corr_and_theoretical_pv.py \
-    -d $dataset \
-    -i $path_to_input \
-    -c $chromosomes \
-    -o ${output_path}/corr_files/theoretical_pv_output \
-    -n $n_cores
 
-python3.9 \
-    ${scripts_path}/02.get_empirical_pv.py \
-    -d $dataset \
-    -i ${output_path}/corr_files/theoretical_pv_output \
-    -o ${output_path}/corr_files/empirical_pv_output \
-
-python3.9 \
-    ${scripts_path}/03.get_CMs_from_edgelist.py \
-    -d $dataset \
-    -corr_p ${output_path}/corr_files/empirical_pv_output/${dataset}_empirical_corr_p_values.txt \
-    -pv ${pvalue} \
-    -t 1 \
-    -f 1 \
-    -m 1 \
-    -o $output_path/CMs
